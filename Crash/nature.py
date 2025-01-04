4d56c5a8f2db8784c16533a26636c3fb6026477e
XenonNek0 <xenonnek0@163.com>
2025-01-04 22:06:22 +0800

    设置了时间显示，与本地时间同步

---
+++ Crash/nature.py
@@ -0,0 +1,18 @@
+import time
+
+
+class Nature:
+    def __init__(self, weather, temperature) -> None:
+        self.time = time.localtime()
+        self.weather = weather
+        self.temperature = temperature
+
+    def show_time(self):
+        formatted_time = time.strftime("%H:%M:%S", self.time)
+        print(formatted_time)
+
+
+if __name__ == "__main__":
+    a = Nature(1, 2)
+    b = a.show_time()
+    print(b)
---
+++ Crash/weather_option.py
@@ -0,0 +1,5 @@
+import random
+
+
+def weather_random():
+
