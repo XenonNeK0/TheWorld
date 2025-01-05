236f24d0b5d5f651648d1ac531872c30853e7690
XenonNek0 <xenonnek0@163.com>
2025-01-05 19:12:54 +0800

    尝试修改

---
+++ Crash/main.py
@@ -0,0 +1,5 @@
+from nature.nature_info import show_nature
+
+
+if __name__ == "__main__":
+    show_nature()
---
+++ Crash/nature/nature_info.py
@@ -0,0 +1,13 @@
+from nature import Nature
+
+
+def show_nature():
+    show_nature_info = Nature()
+    a = show_nature_info.show_time()
+    b = show_nature_info.show_weather()
+    c = show_nature_info.show_temperature()
+    return a, b, c
+
+
+if __name__ == "__main__":
+    show_nature()
